f00cc8e70e67ef85ef465dd062a05ab728ea20f5
Arjun520 <arjun.anduri@gmail.com>
2023-05-09 16:41:18 +0530

    terraform script for load balancer

---
+++ Resource_group.tf
@@ -0,0 +1,7 @@
+resource "azurerm_resource_group" "Terraform-training-1" {
+  name     = "Terraform-training-1"
+  location = "East US"
+}
+
+
+
---
+++ provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.52.0"
+    }
+  }
+}
+ 
+  provider "azurerm"{
+   features {}
+
+  client_id       = "49059005-1a3b-47c7-b93c-f4c97ef8dc83"
+  client_secret   = "Ta_8Q~vlAOLXfcjSm0MXuURRrhmRfv.1KoKkwdpM"
+  tenant_id       = "dc2b843e-ef1b-483b-b1ff-5eb4eeed0833"
+  subscription_id = "de7edfda-1a4b-496b-9305-d985d0fe3493"
+  }
+ 
+
+
+
+
+
+
+
+
+
+
